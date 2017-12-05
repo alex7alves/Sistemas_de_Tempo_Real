47f010c7c695b1eedaf9eb72d0d569e6da8e0d44
alex7alves <alex7alves10@hotmail.com>
2017-12-05 20:08:40 -0300

    Fazendo outro socket pra envio

--- Trabalho final/Testes trabalho/servidor_trab3.cpp
+++ Trabalho final/Testes trabalho/servidor_trab3.cpp
@@ -134,61 +134,72 @@ void Vetorbool(int x, bool *vetor){
 }
 int main(){
 
-    float media;
-    int retorno;
-    bool *vetor = new bool[8];
-    int server_sockfd;
-    u_int yes=1;
-    struct sockaddr_in enviador_address, recebedor_address;
-    socklen_t enviador_len = sizeof(enviador_address), recebedor_len = sizeof(recebedor_address);
+  float media;
+  int retorno;
+  bool *vetor = new bool[8];
+  int server_sockfd;
+  size_t server_len;
+  socklen_t client_len;
+  struct sockaddr_in server_address;
+  struct sockaddr_in client_address;
+
+  unsigned short receber = 9708,enviar = 9808;
+  struct ip_mreq mreq;  // para endere�o multicast
+
+  unlink("server_socket");  // remocao de socket antigo
+  if ( (server_sockfd = socket(AF_INET, SOCK_DGRAM, 0) )  < 0  )  // cria um novo socket
+  {
+    printf(" Houve erro na ebertura do socket ");
+    exit(1);
+  }
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_address.sin_port = htons(receber);
+
+
+  server_len = sizeof(server_address);
+
+  if(  bind(server_sockfd, (struct sockaddr *) &server_address, server_len) < 0 )
+  {
+    perror("Houve error no Bind");
+    exit(1);
+  }
+
+
+  // use setsockopt() para requerer inscri��o num grupo multicast
+  mreq.imr_multiaddr.s_addr=inet_addr(MULTICAST_ADDR);
+  mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+  if (setsockopt(server_sockfd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) {
+    perror("setsockopt");
+    exit(1);
+  }
+     // tem que ter 2 sockets um pra receber e outro pra enviar
     
-    struct ip_mreq mreq;  // para endere�o multicast
+    // socket pra  enviar
+    
+  int sockfd;
+  int len;
+  struct sockaddr_in address;
 
-    unsigned short enviador_porta = 9707, recebedor_porta = 9807;
+  sockfd = socket(AF_INET, SOCK_DGRAM,0);  // criacao do socket
 
-    unlink("server_socket");  // remocao de socket antigo
-    if ( (server_sockfd = socket(AF_INET, SOCK_DGRAM, 0) )  < 0  ){ //Cria um novo socket
-        printf(" Houve erro na ebertura do socket ");
-        exit(1);
-    }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
+  address.sin_port = htons(enviar);
 
-    if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes))<0){ //Cria um novo socket
-        printf(" Houve erro ao reusar o ADDR ");
-        exit(1);
-    }
-
-//    vetor = new bool[8];
-
-    enviador_address.sin_family = AF_INET;
-    enviador_address.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
-    enviador_address.sin_port = htons(enviador_porta);
-
-    recebedor_address.sin_family = AF_INET;
-    recebedor_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    recebedor_address.sin_port = htons(recebedor_porta);
-
-    if(bind(server_sockfd, (struct sockaddr *) &enviador_address, enviador_len) < 0){
-        perror("Houve error no Bind com o enviador");
-        exit(1);
-    }
-
-    // use setsockopt() para requerer inscri��o num grupo multicast
-    mreq.imr_multiaddr.s_addr=inet_addr(MULTICAST_ADDR);
-    mreq.imr_interface.s_addr=htonl(INADDR_ANY);
-    if (setsockopt(server_sockfd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) {
-        perror("setsockopt");
-        exit(1);
-    }
-
+  len = sizeof(address);
+  
     float valor[2] = {0, 1};
   
     while(1){
 
         printf("Servidor esperando ...\n");
-
-        if(recvfrom(server_sockfd, &valor, sizeof(valor), 0, (struct sockaddr *) &enviador_address, &enviador_len) < 0 ){
-            perror(" erro no RECVFROM( )");
-            exit(1);
+        client_len = sizeof(client_address);
+        if(recvfrom(server_sockfd, &valor, sizeof(valor),0,
+                (struct sockaddr *) &client_address, &client_len) < 0 )
+        {
+          perror(" erro no RECVFROM( )");
+          exit(1);
         }
         printf(" Valor recebido foi = %.2f e %.2f\n", valor[0], valor[1]);
         media = (valor[0]+valor[1])/2;
@@ -198,7 +209,7 @@ int main(){
         Vetorbool(retorno,vetor);
         cout << " o retorno foi " << retorno << endl;
 
-        sendto(server_sockfd, vetor, sizeof(vetor), 0, (struct sockaddr *) &recebedor_address, recebedor_len);
+        sendto(sockfd, vetor,sizeof(*vetor),0,(struct sockaddr *) &address, len);
         cout << " o servidor enviou  ";
         for(int i=0;i<8;i++){
             cout << vetor[i] << " ";
